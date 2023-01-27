e779bbc9eabe7270dd96ebda1edf7fa10db7b089
LuisNguyen <flagtick@gmail.com>
2023-01-27 21:02:07 +0700

    [] (Lorence) Init project

---
+++ resources/js/Pages/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <Head>
+        <title>{{ person.name }}</title>
+    </Head>
+
+    <div>
+        <h1>This event is by: {{ person.name }}</h1>
+    </div>
+</template>
+<script setup>
+import { Head } from "@inertiajs/inertia-vue3";
+
+const props = defineProps({
+    person: Object
+})
+</script>
